559a59337cfa2ab6099c0d84b3548c989a3af93e
Sven Braune <copyboy@hotmail.de>
2010-06-15 21:31:00 +0200

    Use different NamedBinaryTag DLL

--- Data/NbtHost.cs
+++ Data/NbtHost.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using NamedBinaryTag;
+using NBT;
 
 namespace obsidian.Data {
 	public class NbtHost : DataHost {
@@ -30,75 +30,68 @@ namespace obsidian.Data {
 		
 		public override Node Load(string filename,out string name) {
 			if (filename==null) { throw new ArgumentNullException("filename"); }
-			TagCompound root = new TagCompound();
-			root.Load(filename,out name);
+			Tag root = Tag.Load(filename);
+			name = root.Name;
 			return TagToNode(root);
 		}
 		public override void Save(string filename,Node root,string name) {
 			if (filename==null) { throw new ArgumentNullException("filename"); }
 			if (root==null) { throw new ArgumentNullException("root"); }
 			if (name==null) { throw new ArgumentNullException("name"); }
-			Check(root,new LinkedList<Node>());
-			((TagCompound)NodeToTag(root)).Save(filename,name);
+			Tag tag = Tag.Create(name);
+			foreach (KeyValuePair<string,Node> kvp in (Dictionary<string,Node>)root.Value)
+				AddNode(tag,kvp.Key,kvp.Value);
+			tag.Save(filename);
 		}
 		
 		private Node TagToNode(Tag tag) {
 			Node node = new Node();
-			switch (tag.GetId()) {
-				case 1: node.Value = (byte)tag; break;
-				case 2: node.Value = (short)tag; break;
-				case 3: node.Value = (int)tag; break;
-				case 4: node.Value = (long)tag; break;
-				case 5: node.Value = (float)tag; break;
-				case 6: node.Value = (double)tag; break;
-				case 7: node.Value = (byte[])tag; break;
-				case 8: node.Value = (string)tag; break;
-				case 9:
-					foreach (Tag t in (TagList)tag)
-						node.Add(TagToNode(t));
+			switch (tag.Type) {
+				case TagType.Byte: node.Value = (byte)tag; break;
+				case TagType.Short: node.Value = (short)tag; break;
+				case TagType.Int: node.Value = (int)tag; break;
+				case TagType.Long: node.Value = (long)tag; break;
+				case TagType.Float: node.Value = (float)tag; break;
+				case TagType.Double: node.Value = (double)tag; break;
+				case TagType.Byte_Array: node.Value = (byte[])tag; break;
+				case TagType.String: node.Value = (string)tag; break;
+				case TagType.List:
+					foreach (Tag t in tag) node.Add(TagToNode(t));
 					break;
-				case 10:
-					foreach (KeyValuePair<string,Tag> kvp in (TagCompound)tag)
-						node[kvp.Key] = TagToNode(kvp.Value);
+				case TagType.Compound:
+					foreach (Tag t in tag) node[t.Name] = TagToNode(t);
 					break;
 			} return node;
 		}
 		
-		private Tag NodeToTag(Node node) {
+		private void AddNode(Tag parent,Node node) {
+			AddNode(parent,null,node);
+		}
+		private void AddNode(Tag parent,string name,Node node) {
 			switch (((IList<Type>)supported).IndexOf(node.Value.GetType())) {
-				case 0: return new TagByte((byte)node.Value);
-				case 1: return new TagShort((short)node.Value);
-				case 2: return new TagInt((int)node.Value);
-				case 3: return new TagLong((long)node.Value);
-				case 4: return new TagFloat((float)node.Value);
-				case 5: return new TagDouble((double)node.Value);
-				case 6: return new TagByteArray((byte[])node.Value);
-				case 7: return new TagString((string)node.Value);
 				case 8:
 					List<Node> l = (List<Node>)node.Value;
-					TagList list = new TagList(ListType(l)??typeof(TagByte));
-					foreach (Node n in l) {
-						if (n.Value==null) { continue; }
-						list.Add(NodeToTag(n));
-					} return list;
+					Tag list = parent.AddList(ListType(l));
+					foreach (Node n in l) AddNode(list,n);
+					break;
 				case 9:
-					TagCompound compound = new TagCompound();
-					foreach (KeyValuePair<string,Node> kvp in (Dictionary<string,Node>)node.Value) {
-						if (kvp.Value.Value==null) { continue; }
-						compound.Add(kvp.Key,NodeToTag(kvp.Value));
-					} return compound;
-			} return null;
+					Dictionary<string,Node> dict = (Dictionary<string,Node>)node.Value;
+					Tag compound = parent.AddCompound();
+					foreach (KeyValuePair<string,Node> kvp in dict)
+						AddNode(parent,kvp.Key,kvp.Value);
+					break;
+				default: parent.Add(node.Value); break;
+			}
 		}
 		
-		private Type ListType(List<Node> list) {
+		private TagType ListType(List<Node> list) {
 			Type type = null;
 			foreach (Node n in list) {
 				if (n.Value==null) { continue; }
 				Type t = n.Value.GetType();
-				t = Tag.FromId((byte)(((IList<Type>)supported).IndexOf(t)+1));
 				if (type==null) { type = t; continue; }
 				if (type!=t) { throw new Exception("List<Node> musn't contain different types of values."); }
-			} return type;
+			} return (TagType)((IList<Type>)supported).IndexOf(type);
 		}
 	}
 }
